29636c188fcb7627a3a0179a1c2b230c11c2c89a
Dennis Kehrig <mailgithub@denniskehrig.de>
2016-07-06 00:39:24 -0700

    Simplified auto calibration. I though due to float vs. double precision it might be better to accumulate the distance moved in a double variable, but float appears to be precise enough, so we need to maintain less state.

--- Assets/MouseMovement.cs
+++ Assets/MouseMovement.cs
@@ -2,49 +2,34 @@
 using System.Collections;
 
 public class MouseMovement : MonoBehaviour {
-    const float scaling = 0.05f;
-    const float minStableTable = 1f;
-    
     float y, minY, maxY, centerY;
-    float lastMinChange, lastMaxChange;
-    double distanceMoved;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         centerY = transform.position.y;
         y = 0;
-        distanceMoved = 0;
         minY = 0;
         maxY = 0;
-        lastMinChange = Time.time;
-        lastMaxChange = Time.time;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         float relY = Input.GetAxis("Mouse Y");
         if (relY == 0)
             return;
+
         y += relY;
-        distanceMoved += Mathf.Abs(relY);
-        float buffer = (float)(distanceMoved * 0.01d);
+        float buffer = Mathf.Abs(relY) * 0.02f;
+        minY += buffer;
+        maxY -= buffer;
 
-        float effectiveMinY = minY + buffer;
-        float effectiveMaxY = maxY - buffer;
+        if (y < minY)
+            minY = y;
+        else if (y > maxY)
+            maxY = y;
 
-        if (y < effectiveMinY) {
-            minY = effectiveMinY = y;
-            maxY = effectiveMaxY;
-            distanceMoved = 0;
-        }
-        else if (y > effectiveMaxY) {
-            maxY = effectiveMaxY = y;
-            minY = effectiveMinY;
-            distanceMoved = 0;
-        }
-
-        float range = effectiveMaxY - effectiveMinY;
-        float progress = (range == 0) ? 0.5f : ((y - effectiveMinY) / range);
+        float range = maxY - minY;
+        float progress = (range == 0) ? 0.5f : ((y - minY) / range);
 
         transform.position = new Vector3(transform.position.x, centerY + (progress - 0.5f), transform.position.z);
     }
