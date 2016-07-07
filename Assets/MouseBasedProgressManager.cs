61b9cebe559e1eb1a53ff559fedd8113bd4d7dfa
Dennis Kehrig <mailgithub@denniskehrig.de>
2016-07-06 22:03:11 -0700

    Split mouse input to progress conversion, and application of said conversion to movement intot separate scripts

---
+++ Assets/MouseBasedProgressManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseBasedProgressManager : MonoBehaviour {
+    float y, minY, maxY;
+
+    public float Progress { get; private set; }
+    public float ProgressChange { get; private set; }
+
+    // Use this for initialization
+    void Start() {
+        y = 0;
+        minY = 0;
+        maxY = 0;
+        Progress = 0.5f;
+        ProgressChange = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float relY = Input.GetAxis("Mouse Y");
+        if (relY == 0) {
+            ProgressChange = 0.0f;
+            return;
+        }
+
+        y += relY;
+        float buffer = Mathf.Abs(relY) * 0.015f;
+        minY += buffer;
+        maxY -= buffer;
+
+        if (y < minY)
+            minY = y;
+        else if (y > maxY)
+            maxY = y;
+
+        float range = maxY - minY;
+        float progress = (range == 0) ? 0.5f : ((y - minY) / range);
+        ProgressChange = progress - Progress;
+        Progress = progress;
+    }
+}
--- Assets/MouseMovement.cs
+++ Assets/MouseMovement.cs
@@ -2,35 +2,21 @@
 using System.Collections;
 
 public class MouseMovement : MonoBehaviour {
-    float y, minY, maxY, centerY;
+    public GameObject managers;
+
+    MouseBasedProgressManager progressManager;
+    float centerY;
 
     // Use this for initialization
     void Start() {
+        Debug.Log("Managers: " + managers);
+        progressManager = managers.GetComponent<MouseBasedProgressManager>();
+        Debug.Log("Progress manager: " + progressManager);
         centerY = transform.position.y;
-        y = 0;
-        minY = 0;
-        maxY = 0;
     }
 
     // Update is called once per frame
     void Update() {
-        float relY = Input.GetAxis("Mouse Y");
-        if (relY == 0)
-            return;
-
-        y += relY;
-        float buffer = Mathf.Abs(relY) * 0.02f;
-        minY += buffer;
-        maxY -= buffer;
-
-        if (y < minY)
-            minY = y;
-        else if (y > maxY)
-            maxY = y;
-
-        float range = maxY - minY;
-        float progress = (range == 0) ? 0.5f : ((y - minY) / range);
-
-        transform.position = new Vector3(transform.position.x, centerY + (progress - 0.5f), transform.position.z);
+        transform.position = new Vector3(transform.position.x, centerY + (progressManager.Progress - 0.5f), transform.position.z);
     }
 }
