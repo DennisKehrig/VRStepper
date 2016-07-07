6f6fb4b43dd6e33a7dc55e29c402ec0e7a62dcf2
Dennis Kehrig <mailgithub@denniskehrig.de>
2016-07-06 22:20:17 -0700

    Use absolute progress change (regardless of direction) to lift

---
+++ Assets/ApplyProgressChangeToY.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyProgressChangeToY : MonoBehaviour
+{
+    public GameObject managers;
+
+    MouseBasedProgressManager progressManager;
+    float initialY;
+
+    // Use this for initialization
+    void Start() {
+        progressManager = managers.GetComponent<MouseBasedProgressManager>();
+        initialY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(Vector3.up * 0.1f * Mathf.Abs(progressManager.ProgressChange));
+    }
+}
