bb88e0a76068a6838eacc57c56b8ec50ca80b02f
Dennis Kehrig <mailgithub@denniskehrig.de>
2016-07-08 23:12:38 -0700

    Moving the model with the progress

---
+++ Assets/Scripts/ApplyProgressToModel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyProgressToModel : MonoBehaviour {
+    const float minAngle = -0.75f;
+    const float maxAngle = 25;
+
+    public GameObject Managers;
+    public GameObject LeftPedal;
+    public GameObject RightPedal;
+
+    MouseBasedProgressManager progressManager;
+    
+    // Use this for initialization
+    void Start () {
+        progressManager = Managers.GetComponent<MouseBasedProgressManager>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float progress = progressManager.Progress;
+        RightPedal.transform.eulerAngles = new Vector3(minAngle +      progress  * (maxAngle - minAngle), 0, 0);
+        LeftPedal.transform.eulerAngles  = new Vector3(minAngle + (1 - progress) * (maxAngle - minAngle), 0, 0);
+    }
+}
